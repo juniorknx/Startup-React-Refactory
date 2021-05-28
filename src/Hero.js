6e530e7a565eb121c4fde8b9a5a92ca3371fc2bf
Julio Junior <juniorknx@hotmail.com>
2021-05-28 00:45:39 -0300

    Add fade-in effects

--- src/Hero.js
+++ src/Hero.js
@@ -1,20 +1,23 @@
 import '../src/css/hero.css';
 import hero from '../src/assets/hero.svg';
 import Button from '../src/Button';
+import FadeIn from 'react-fade-in';
 
 function Hero () {
     return (
-        <div className="hero">
-            <div className="img-hero">
-                <img src={hero} alt="hero" />
-            </div>
+        <FadeIn transitionDuration="800">
+            <div className="hero">
+                <div className="img-hero">
+                    <img src={hero} alt="hero" />
+                </div>
 
-            <div className="hero-info">
-                <h3>Alavanque seu negócio com um</h3>
-                <h1>Website Incrível</h1>
-                <Button title="Solicitar Orçamento" />
+                <div className="hero-info">
+                    <h3>Alavanque seu negócio com um</h3>
+                    <h1>Website Incrível</h1>
+                    <Button title="Solicitar Orçamento" />
+                </div>
             </div>
-        </div>
+        </FadeIn>
     );
 }
 
