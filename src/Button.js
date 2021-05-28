522792dfd0f24236996c2d9363bfd3acce7522bb
Julio Junior <juniorknx@hotmail.com>
2021-05-28 00:17:22 -0300

    Refactory

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import '../src/css/button.css';
+
+function Button (props) {
+    return <button className="btn-primary" >{props.title}</button>
+}
+
+export default Button
--- src/Hero.js
+++ src/Hero.js
@@ -1,5 +1,6 @@
 import '../src/css/hero.css';
 import hero from '../src/assets/hero.svg';
+import Button from '../src/Button';
 
 function Hero () {
     return (
@@ -11,6 +12,7 @@ function Hero () {
             <div className="hero-info">
                 <h3>Alavanque seu negócio com um</h3>
                 <h1>Website Incrível</h1>
+                <Button title="Solicitar Orçamento" />
             </div>
         </div>
     );
