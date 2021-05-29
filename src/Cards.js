cc9c85d81028100eb0cde03379321031029239cb
Julio Junior <juniorknx@hotmail.com>
2021-05-29 11:51:54 -0300

    props images

--- src/Cards.js
+++ src/Cards.js
@@ -1,8 +1,10 @@
 import '../src/css/services.css';
 
+
 function Cards (props){
     return (
         <div className="cards">
+            <img src={props.image} style={{width:70, marginBottom:10}}/>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
         </div>
