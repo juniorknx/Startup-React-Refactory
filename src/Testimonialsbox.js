b678a2f80e5d3a113262ef221370c18d01c8e6e9
Julio Junior <juniorknx@hotmail.com>
2021-05-31 20:01:34 -0300

    add testimonials box component

---
+++ src/Testimonialsbox.js
@@ -0,0 +1,19 @@
+import '../src/css/testimonials.css';
+
+function TestimonialsBox (props){
+    return (
+        <div className="box">
+                <div className="avatar">
+                    <img src={props.avatar} />
+                </div>
+
+                <div className="cards-description">
+                    <p>{props.description}</p>
+                    <h2>{props.name}</h2>
+                    <h3>{props.company}</h3>
+                </div>
+            </div>
+    );
+}
+
+export default TestimonialsBox
